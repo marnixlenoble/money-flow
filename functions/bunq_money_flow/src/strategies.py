197dde7ec94559773f34b83ad900977f1f97dc1d
marnix le noble <marnix.lenoble@gmail.com>
2024-05-07 09:30:46 +0200

    chore: improve logging

--- functions/bunq_money_flow/src/strategies.py
+++ functions/bunq_money_flow/src/strategies.py
@@ -1,3 +1,4 @@
+import logging
 from decimal import Decimal
 from typing import Optional
 
@@ -11,27 +12,46 @@ def _check_minimum_amount(
     if amount > (minimum_amount if minimum_amount else 0):
         return amount
     else:
+        logging.info(
+            f"\t Amount {amount} is less than minimum amount {minimum_amount}. Skipping."
+        )
         return Decimal(0)
 
 
 def _check_maximum_amount(
     amount: Decimal, *, maximum_amount: Optional[Decimal]
 ) -> Decimal:
-    if maximum_amount is not None:
-        return min(amount, maximum_amount)
+    if maximum_amount is not None and amount > maximum_amount:
+        logging.info(
+            f"\t Amount {amount} exceeds maximum amount {maximum_amount}. Using maximum amount."
+        )
+        return maximum_amount
     else:
         return amount
 
 
 def _check_remainder(amount: Decimal, *, remainder: Decimal) -> Decimal:
-    return min(amount, remainder)
+    if amount < remainder:
+        return amount
+    else:
+        logging.info(
+            f"\t Amount {amount} exceeds remainder {remainder}. Using remainder."
+        )
+        return remainder
 
 
 def top_up_strategy(
     allocation: Allocation, remainder: Decimal, *, bank_client: BankClient
 ) -> Decimal:
+    logging.info(
+        f"Attempting to top up allocation {allocation.description} to {allocation.value}."
+    )
     balance = bank_client.get_balance_by_iban(iban=allocation.target_iban)
     amount = allocation.value - balance
+    if amount < 0:
+        logging.info(f"\t Skipping allocation. Balance is already sufficient.")
+        return Decimal(0)
+
     amount = _check_remainder(amount, remainder=remainder)
     amount = _check_minimum_amount(amount, minimum_amount=allocation.minimum_amount)
     amount = _check_maximum_amount(amount, maximum_amount=allocation.maximum_amount)
@@ -40,6 +60,9 @@ def top_up_strategy(
 
 
 def fixed_strategy(allocation: Allocation, remainder: Decimal, *_, **__) -> Decimal:
+    logging.info(
+        f"Attempting to allocate {allocation.value} to {allocation.description}."
+    )
     amount = allocation.value
     amount = _check_remainder(amount, remainder=remainder)
     amount = _check_minimum_amount(amount, minimum_amount=allocation.minimum_amount)
@@ -51,6 +74,9 @@ def fixed_strategy(allocation: Allocation, remainder: Decimal, *_, **__) -> Deci
 def percentage_strategy(
     allocation: Allocation, remainder: Decimal, *_, **__
 ) -> Decimal:
+    logging.info(
+        f"Attempting to allocate {allocation.value}% to {allocation.description}."
+    )
     amount = round(remainder * (allocation.value / 100), 2)
     amount = _check_minimum_amount(amount, minimum_amount=allocation.minimum_amount)
     amount = _check_maximum_amount(amount, maximum_amount=allocation.maximum_amount)
